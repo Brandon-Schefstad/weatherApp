ddc35b746ee19f5f2c5cb52c5decaefa3377d5a4
Brandon-Schefstad <bschefstad@gmail.com>
2022-04-10 02:46:27 -0400

    Working product

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-//Make an api request using async await
+// async function fetchWeather(){
+//   let url = 'https://api.openweathermap.org/data/2.5/weather?lat=27.9477595&lon=-82.458444&appid=a6211e4c7a048dd89e23d0e4ef2c9326&units=imperial'
+//   try{
+//     let response = await fetch(url)
+//     return await response.json();
+//   }catch(error){
+//     console.log(error)
+//   }
+// }
+document.querySelector('.submit').addEventListener('click', updateDOM)
 
-async function fetchWeather(){
-  let url = 'https://api.openweathermap.org/data/2.5/weather?lat=27.9477595&lon=-82.458444&appid=a6211e4c7a048dd89e23d0e4ef2c9326&units=imperial'
+// User input city/state
+async function userFetchWeather(){
+  const city = document.querySelector('#city').value 
+  const state = document.querySelector('#state').value
+  let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&appid=a6211e4c7a048dd89e23d0e4ef2c9326&units=imperial`
   try{
     let response = await fetch(url)
     return await response.json();
@@ -9,8 +21,43 @@ async function fetchWeather(){
     console.log(error)
   }
 }
-async function updateDOM() {
-  let information = await fetchWeather();
-  document.querySelector('div').innerHTML = `${JSON.stringify(information)}`;
+// Uses city/state input to get lat/lon for weather data
+async function getWeatherData() {
+  let information = await userFetchWeather();
+  document.querySelector('.maindisplay__text').innerText =`City Name: ${information[0].name}`
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${information[0].lat}&lon=${information[0].lon}&appid=a6211e4c7a048dd89e23d0e4ef2c9326&units=imperial`
+  try{
+    let data = await fetch(url)
+    data = await data.json()
+    document.querySelector('.temperature').innerHTML=`Temperature: ${data.main.temp} °F`
+    document.querySelector('.feels_like').innerText=`That feels like: ${data.main.feels_like}°F`
+    const visibility = (data.visibility/100)
+    document.querySelector('.visibility').innerText=`Visibility: ${visibility}%`
+    document.querySelector('.description').innerText=`Description: ${data.weather[0].description}`
+  }catch(error){
+    console.error(error)
+  }  
 }
-updateDOM()
+// Uses city name to get image 
+async function getImage(){
+  let city = document.querySelector('#city').value 
+  city = city.toLowerCase()
+  let url = `https://api.teleport.org/api/urban_areas/slug:${city}/images/`
+  console.log(`url for ${city} ${url}`)
+  try{
+    document.querySelector('img').classList.remove('hidden')
+    let response = await fetch(url)
+    response = await response.json()
+    let source = response.photos[0].image.mobile
+    console.log(source)
+    document.querySelector('img').src = response.photos[0].image.mobile
+  }catch(error){
+    console.error(error)
+    document.querySelector('img').classList.add('hidden')
+  }
+}
+async function updateDOM(){
+  getWeatherData()
+  getImage()
+}
+
