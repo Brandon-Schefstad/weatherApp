8662e048b5ee79f11269abf1b70182b21cbfab18
Brandon-Schefstad <bschefstad@gmail.com>
2022-04-10 02:54:37 -0400

    Added https

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ document.querySelector('.submit').addEventListener('click', updateDOM)
 async function userFetchWeather(){
   const city = document.querySelector('#city').value 
   const state = document.querySelector('#state').value
-  let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&appid=a6211e4c7a048dd89e23d0e4ef2c9326&units=imperial`
+  let url = `https://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&appid=a6211e4c7a048dd89e23d0e4ef2c9326&units=imperial`
   try{
     let response = await fetch(url)
     return await response.json();
